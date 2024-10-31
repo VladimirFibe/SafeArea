cbb011553e8b4fed1891d6bf4b0e999171bfd100
Vladimir Fibe <motiw@me.com>
2024-10-31 15:49:14 +0500

    Initial Commit

---
+++ SafeArea/SafeAreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeAreaApp.swift
+//  SafeArea
+//
+//  Created by Vladimir Fibe on 31.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SafeAreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
